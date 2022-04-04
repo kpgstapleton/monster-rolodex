b59411c355ea43b085a90d6f6097aaa1dcd474f6
Kevin Stapleton <kpgstapleton@gmail.com>
2022-04-04 13:57:57 +0100

    setstate and secondary callback

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-
 import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
+class App extends Component {
 
-  render(){
+  constructor(){
+    super();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      name: {firstName: 'Kevin', lastName: 'Stapleton'},
+      company: 'KSD'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p> Hello I'm {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
+          <button onClick={()=>{
+            (this.setState(
+              () => {
+                return {
+                  name: {firstName: 'Molly', lastName: 'Stapleton'}
+                }
+              }, 
+              ()=>{
+                console.log(this.state);
+              }
+            ))
+          }}>
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
   }
 }
 
