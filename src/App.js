983cbd058aeea8dd4f60adf07e0121fb74e53d56
Kevin Stapleton <kpgstapleton@gmail.com>
2022-04-06 11:14:10 +0100

    optimisation

--- src/App.js
+++ src/App.js
@@ -23,25 +23,29 @@ class App extends Component {
       }));
   }
 
+  onSearchChange= (event)=>{
+    const searchField = event.target.value.toLocaleLowerCase();
+    
+    this.setState(()=>{
+
+      return {searchField};
+    })
+  }
+
   render() {
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const {monsters,searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
+
 
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' 
-        onChange={ 
-          (event)=>{
-            const searchField = event.target.value.toLocaleLowerCase();
-            
-            this.setState(()=>{
-
-              return {searchField};
-            })
-          }
-      }
+        onChange={onSearchChange}
          />
         {
           
